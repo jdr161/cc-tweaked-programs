ed599bf8d5d7726dc7eae41ce72d27a2df8a05ce
James Redding <98284325+jdr161@users.noreply.github.com>
2024-11-07 21:19:09 -0600

    Pull Dimensional Miner when Low Durability (#1)
    * initial pull mineshaft
    
    * added sleep and move item success bool
    
    * handle full durability case
    
    * fix comparison
    
    ---------
    
    Co-authored-by: jdr161 <james.redding40@yahoo.com>

---
+++ pull_mineshaft.lua
@@ -0,0 +1,54 @@
+function GetMineshaftInventory()
+    for _, name in pairs(peripheral.getNames()) do
+        if peripheral.hasType(name, "occultism:dimensional_mineshaft") then
+            return peripheral.wrap(name)
+        end
+    end
+    return nil
+end
+
+function GetDurability(inventory, slot)
+    if inventory == nil or slot == nil or inventory.getItemDetail(slot) == nil or inventory.getItemDetail(slot).durability == nil then
+        return -1
+    end
+    return inventory.getItemDetail(slot).durability
+end
+
+function IsLowDurability(durability)
+    if durability == nil then
+        return false
+    end
+    return 0 < durability and durability <= 0.05
+end
+
+function TryMoveItem(fromInventory, fromSlot, toInventory)
+    if fromInventory == nil or fromSlot == nil or toInventory == nil then
+        return false
+    else
+        fromInventory.pushItems(peripheral.getName(toInventory), fromSlot)
+        return true
+    end
+end
+
+function GetNonShaftAdjacentInventory()
+    for _, name in pairs(peripheral.getNames()) do
+        if peripheral.hasType(name, "inventory") and not peripheral.hasType(name, "occultism:dimensional_mineshaft") then
+            return peripheral.wrap(name)
+        end
+    end
+    return nil
+end
+
+function Main()
+    while true do
+        local mineshaftInventory = GetMineshaftInventory()
+        if IsLowDurability(GetDurability(mineshaftInventory, 1)) then
+            if TryMoveItem(mineshaftInventory, 1, GetNonShaftAdjacentInventory()) then
+                print("Moved item with low durability")
+            end
+        end
+        sleep(20)
+    end
+end
+
+Main()
